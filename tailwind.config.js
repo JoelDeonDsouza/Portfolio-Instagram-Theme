f2e8b62dc5b5bed42cb085aea47591ff0fed452d
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-10-26 00:26:07 +0200

    error in production phase

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
